b616becbc86c6184c7ca506cc03032f6c9fce569
plapicola <p.lapicola@gmail.com>
2019-06-14 00:06:26 -0600

    Adds testing for event medalist specs

--- spec/models/event_medalist_spec.rb
+++ spec/models/event_medalist_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe EventMedalist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Relationships' do
+    it {should belong_to :olympian}
+    it {should belong_to :event}
+  end
 end
