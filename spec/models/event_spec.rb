624598ba2760bf6b47abce9fd391dc70b7288f42
plapicola <p.lapicola@gmail.com>
2019-06-14 00:02:30 -0600

    Adds event relationship testing

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Relationships' do
+    it {should belong_to :sport}
+    it {should have_many :olympian_events}
+    it {should have_many :event_medalists}
+    it {should have_many(:olympians).through(:olympian_events)}
+  end
 end
