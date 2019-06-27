f4ab80530a8a92f5f7b75cbb44fac4996df259aa
plapicola <p.lapicola@gmail.com>
2019-06-27 10:25:03 -0600

    Backfills missed unit test for olympian#all_with_medals

--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -13,4 +13,30 @@ RSpec.describe Olympian, type: :model do
     it {should define_enum_for(:sex).with_values(%i(male female))}
   end
 
+  describe 'Class Methods' do
+
+    before :each do
+      @shooting = create(:sport, name: "Shooting")
+      @olympian_1 = create(:olympian, name: "Mike", age: 27)
+      @olympian_2 = create(:olympian, name: "Peter", age: 25)
+   end
+
+    describe 'all_with_medals' do
+      before :each do
+        @skeet = create(:event, name: "Skeet")
+        @mike_gold = EventMedalist.create(event: @skeet, olympian: @olympian_1, medal: :gold)
+      end
+
+      it 'should return all olympians in the system with their medal count' do
+        olympians = Olympian.all_with_medals
+        
+        expect(olympians.length).to eq(2)
+        expect(olympians[0]).to eq(@olympian_1)
+        expect(olympians[0].total_medals_won).to eq(1)
+        expect(olympians[1]).to eq(@olympian_2)
+        expect(olympians[1].total_medals_won).to eq(0)
+      end
+    end
+  end
+
 end
