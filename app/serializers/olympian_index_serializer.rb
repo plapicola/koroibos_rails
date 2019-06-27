00cdca4e597c5e68c6a8149d7ade19cb85eca245
plapicola <p.lapicola@gmail.com>
2019-06-27 10:18:53 -0600

    Adds olympian index serializer

---
+++ app/serializers/olympian_index_serializer.rb
@@ -0,0 +1,17 @@
+class OlympianIndexSerializer
+   class << self
+      def format_all(olympians)
+         olympians.map {|olympian| format_one(olympian)}
+      end
+
+      def format_one(olympian)
+         {
+            name: olympian.name,
+            team: olympian.team.name,
+            age: olympian.age,
+            sport: olympian.sport.name,
+            total_medals_won: olympian.total_medals_won
+         }
+      end
+   end
+end
