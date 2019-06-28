16434baa0df069131f54c7036abe06a19d6487f1
plapicola <p.lapicola@gmail.com>
2019-06-27 22:41:59 -0600

    Adds testing for youngest olympian query parameter

--- spec/requests/olympian_index_spec.rb
+++ spec/requests/olympian_index_spec.rb
@@ -30,6 +30,21 @@ RSpec.describe 'Olympian API', type: :request do
             expect(olympians[1][:team]).to eq(@olympian_2.team.name)
             expect(olympians[1][:total_medals_won]).to eq(0)
          end
+
+         it 'can optionally filter by youngest olympian' do
+            get '/api/v1/olympians?age=youngest'
+
+            olympians = JSON.parse(response.body, symbolize_names: true)[:olympians]
+
+            expect(olympians).to be_a Array
+            expect(olympians.length).to eq(1)
+            expect(olympians[0]).to be_a Hash
+            expect(olympians[0][:name]).to eq(@olympian_2.name)
+            expect(olympians[0][:age]).to eq(@olympian_2.age)
+            expect(olympians[0][:sport]).to eq(@olympian_2.sport.name)
+            expect(olympians[0][:team]).to eq(@olympian_2.team.name)
+            expect(olympians[0][:total_medals_won]).to eq(1)
+         end
       end
    end
 end
